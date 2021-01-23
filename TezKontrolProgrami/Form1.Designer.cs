ff7c174bad12d7d41d4b9106ef6c370bc174899f
gulecfatih <56516242+gulecfatih@users.noreply.github.com>
2021-01-23 16:05:19 +0300

    Proje dosyası ekle.

---
+++ TezKontrolProgrami/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace TezKontrolProgrami
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(559, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dosyayı çıkar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Dosya Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(291, 502);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(374, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(464, 425);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(82, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(286, 425);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(847, 499);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Rapor";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(847, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 425);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rapor";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(18, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(249, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Önsözde ilk paragrafta teşekkür ibaresi var mı ?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 587);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TezKontrolProgrami/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TezKontrolProgrami
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible = false;
+            label2.Visible = false;
+            checkBox1.Visible = false;
+        }
+        OpenFileDialog file = new OpenFileDialog();
+
+        string Dosyayolu = "";
+       
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Word Dosyası |*.docx";
+            file.ShowDialog();
+            Dosyayolu = file.FileName;
+            Dosyayolu = Dosyayolu.Replace("\\", "/");
+            textBox2.Text = Dosyayolu;
+        } // dosyaları c# aktarma
+
+
+        List<string> bilgi = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            richTextBox1.Text = "";
+            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+            object miss = System.Reflection.Missing.Value;
+            object path = textBox2.Text;
+            object readOnly = true;
+            Microsoft.Office.Interop.Word.Document docs = word.Documents.Open(ref path, ref miss, ref readOnly, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss);
+            string totaltext = "";
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.Columns[0].Name = "Bilgiler";
+
+            
+
+            int j = 0;
+            for (int i = 0; i < docs.Paragraphs.Count; i++)
+            {
+                
+                if (docs.Paragraphs[i + 1].Range.Font.Size == 13 &&docs.Paragraphs[i + 1].Range.Font.Bold==-1 && docs.Paragraphs[i + 1].Range.Text.ToString() != "\r" && docs.Paragraphs[i + 1].Range.Text.ToString() != "\n")
+                {
+                    if (j == 0)// ilk gelen verinin kapak yazısı olmasını sağlama
+                    {
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[j].Cells[0].Value = "Kapak";
+                        bilgi.Add(totaltext);
+                        totaltext = "";
+                        j++;
+                        dataGridView1.Rows[j].Cells[0].Value = docs.Paragraphs[i + 1].Range.Text.ToString();
+                        j++;
+                    }
+                    else // 2. gelen başlığın adını çekmek
+                    {
+                        bilgi.Add(totaltext);
+                        totaltext = "";
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[j].Cells[0].Value = docs.Paragraphs[i + 1].Range.Text.ToString(); // başlığı datagridview aktar
+                        j++;
+                    }
+                    
+                }
+                else // eğer gelen veri başlık değilse alt başlık olarak almayı sağlamak için kurulan yapı
+                {
+                    totaltext+= docs.Paragraphs[i + 1].Range.Text.ToString(); // alt başlığı çekme
+                }
+            }
+            bilgi.Add(totaltext);
+            MessageBox.Show("Dosya Aktarımı Başarıyla gerçekleşmiştir", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        } // dosya yolu seçme
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "";
+                richTextBox1.Text = bilgi[dataGridView1.CurrentRow.Index];
+            }
+            catch
+            {
+                MessageBox.Show("Boş Satır Seçtiniz Lütfen Dolu Satırlardan birini Seçin", "Bilgilendirme", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        } // alt metinlere inme
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            label1.Visible = true;
+            label2.Visible = true;
+            checkBox1.Visible = true;
+
+
+            string kaynakca = AltBaslıgıBulma("KAYNAKLAR");
+            int kaynaksayisi = Kaynaksayısı(kaynakca);
+            label1.Text = kaynaksayisi.ToString() + "Kaynak Sayısı  ";
+
+            //çift tırnak kontrolü
+            int cifttirnak = 0;
+            cifttirnak = ciftTirnak();
+            label2.Text = "Çift tırnak içinde 50 den fazla kelime olan söz sayısı" + cifttirnak;
+
+
+            //önsöz ilk paragrafında teşekkür kelimesi var mı ?
+            string onsoz = AltBaslıgıBulma("ÖNSÖZ");
+            bool Cevap = onsozTesekkur(onsoz);
+            checkBox1.Enabled = false;
+            checkBox1.Checked = Cevap;
+           
+        } // raporlama
+
+        private bool onsozTesekkur(string onsoz)
+        {
+            bool cevap;
+            string tut="";
+            for(int i = 0; i < onsoz.Length; i++)
+            {
+                tut += onsoz[i];
+                try
+                {
+                    if (onsoz[i] =='.' && onsoz[i+1]=='\r')
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            int sayi =tut.IndexOf("teşekkür", 0, tut.Length);
+
+            if (sayi == -1)
+                cevap = false;
+            else
+                cevap = true;
+            return cevap;
+        } // Onsoz ilk paragraf kontrol
+
+        public int ciftTirnak()
+        {
+            int tirnakuygunmu = 0;
+            foreach (string eleman in bilgi)
+            {
+                string Eleman;
+                Eleman = eleman;
+                int sayac = 0;
+                bool giris = false;
+                for(int i = 0; i < Eleman.Length; i++)
+                {
+                    
+
+                    if (Eleman[i] == '“'||giris==true)
+                    {
+                        giris = true;
+                        Console.WriteLine(Eleman[i]);
+                        sayac++;
+                    }
+                    if (Eleman[i] == '”'||giris==false)
+                    {
+                        giris = false;
+                        if (sayac > 50)
+                        {
+                            tirnakuygunmu++;
+                        }
+                        sayac = 0;
+                    }
+                }
+
+            }
+            return tirnakuygunmu;
+        } //çift tırnak kontrol
+
+
+
+        public string AltBaslıgıBulma(string baslık)
+        {
+            baslık += "\r";
+            int index=0;
+            for (int i = 0; i < dataGridView1.RowCount-1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString() == baslık)
+                {
+                    index = i+1;
+                }
+            }
+            int j = 0;
+            string Eleman="";
+            foreach (string eleman in bilgi)
+            {
+                j++;
+                if (index == j)
+                    Eleman = eleman;
+                
+            }
+            return Eleman;
+
+
+
+
+        } // alt başlıkları bulma 
+
+
+
+        public int Kaynaksayısı(string kaynakca)
+        {
+            int sayac = 0;
+            
+            for(int i = 0; i < kaynakca.Length; i++)
+            {
+                if (kaynakca[i] == '-')
+                {
+                    if(char.IsDigit(kaynakca[i - 1]) == true)
+                    {
+                        if(kaynakca[ i + 1] == ')')
+                        {
+                            sayac++;
+                        }
+                    }
+                    
+                    
+                }
+            }
+            return sayac;
+        } // kaynak sayısını bulma
+    }
+}
+
+
+
+
